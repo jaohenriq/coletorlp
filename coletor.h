58d185608f2c62e6e84a6e2380b6658306da14a6
João Henrique <75077479+jaohenriq@users.noreply.github.com>
2022-05-04 19:49:54 -0300

    Update coletor.h

--- coletor.h
+++ coletor.h
@@ -1,1 +1,36 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE_HEAP 100
+
+typedef struct structPoint {
+    int select;
+    struct structPoint *next;
+    int value;
+    
+}   Point;
+
+typedef struct {
+    Point *heap[SIZE_HEAP];
+    int size_heap;
+}   Heap;
+
+Heap *creatHeap();
+
+void pushPoint(Heap *heap, Point *point);
+
+Objeto *pop(Heap *heap);
+
+void mark(Point *point);
+
+void mark_heap(Heap *heap);
+
+void sweep(Heap *heap);
+
+void coletor(Heap *heap);
+
+Objeto *criaObj(Heap *heap);
+
+void push(Heap *heap, int val);
+
+void freeheap(Heap *heap);
